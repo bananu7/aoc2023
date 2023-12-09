e08199bbdfef7462a34b393a649803a463b11ce8
Bartek Banachewicz <bartosz.banachewicz@airspace-intelligence.com>
2023-12-09 11:21:59 +0100

    day9p1

---
+++ day9.py
@@ -0,0 +1,36 @@
+def cosequence(seq):
+    coseq = []
+    for i in range(0, len(seq) - 1):
+        coseq.append(seq[i + 1] - seq[i])
+    return coseq
+
+
+def all_zero(seq):
+    return all(map(lambda x: x == 0, seq))
+
+
+def solve(seq):
+    if all_zero(seq):
+        return 0
+    else:
+        return seq[-1] + solve(cosequence(seq))
+
+
+def main():
+    file = open("input9.txt", "r")
+    lines = file.readlines()
+
+    res_sum = 0
+
+    for line in lines:
+        line = line.strip()
+        nums = list(map(int, line.split(" ")))
+
+        result = solve(nums)
+        res_sum += result
+        print(result)
+
+    print(res_sum)
+
+
+main()
